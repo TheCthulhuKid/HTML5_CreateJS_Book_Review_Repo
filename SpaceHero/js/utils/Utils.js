d012785272bacc76414e4826f844d9afee9fb16c
gileadslostson <gileadslostson@gmail.com>
2014-05-27 15:31:00 +0200

    Code for the 3rd game project

---
+++ SpaceHero/js/utils/Utils.js
@@ -0,0 +1,13 @@
+(function () {
+
+    var Utils = {
+
+    }
+
+    Utils.getRandomNumber = function (min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    window.Utils = Utils;
+
+}());
